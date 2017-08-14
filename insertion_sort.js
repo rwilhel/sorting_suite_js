933c0c8d31a83ebf8e9dd8c3e21cf05ab1dfbad7
rwilhel <rwilhel@gmail.com>
2017-08-13 20:21:18 -0600

    Passes test for sorting an array of 2 letters with Insertion Sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,7 +1,20 @@
 var mixedUp = ["d", "b", "a", "c"];
 
 function insertionSort(mixedUp) {
-
-};
+  var length = mixedUp.length;
+  for(var i = 1; i < length; i++) {
+    var temp = mixedUp[i];
+    for(var j = i - 1; j >= 0; j--) {
+      if (mixedUp[j] > temp) {
+        mixedUp[j + 1] = mixedUp[j];
+      }
+      else {
+        break;
+      }
+    }
+    mixedUp[j + 1] = temp;
+  }
+  return mixedUp;
+}
 
 module.exports = insertionSort
--- test/insertion_sort_test.js
+++ test/insertion_sort_test.js
@@ -4,3 +4,7 @@ const insertionSort = require('../insertion_sort');
 it('is a function', function() {
   assert.isFunction(insertionSort, 'this is a function');
 });
+
+it('can sort an array of 2 letters', function() {
+  assert.deepEqual(insertionSort(["z", "a"]), ["a", "z"]);
+});
